1e5b042cc293608f47564369e9f0570b06b2b8cb
yoko067 <ee217067@meiji.ac.jp>
2024-05-23 20:07:33 +0900

    useStateを用いて値(X)を保持し、盤面上にその値を表示する機能を追加

--- react-intro/tutorial2/src/App.js
+++ react-intro/tutorial2/src/App.js
@@ -1,5 +1,19 @@
-function Square({ value }) {
-  return <button className="square">{value}</button>
+import { useState } from 'react';
+
+function Square() {
+  const [value, setValue] = useState(null); // 初期値はnull
+
+  function handleClick(){
+    setValue("X");
+  }  
+  return (
+  <button 
+    className="square"
+    onClick={handleClick}
+  >
+    {value}
+  </button>
+ );
 }
 
 export default function Board() {

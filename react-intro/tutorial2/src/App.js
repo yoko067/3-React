ca3317b192a099f4751f0f78ace3a0f6fe903cea
yoko067 <ee217067@meiji.ac.jp>
2024-05-24 17:23:49 +0900

    <add>: ゲームの勝利条件を設定し、判定する機能と現在の状況を表示する機能を追加

--- react-intro/tutorial2/src/App.js
+++ react-intro/tutorial2/src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 
+// 一つのマスについての関数
 function Square({value, onSquareClick}) {
 
   return (
@@ -12,13 +13,15 @@ function Square({value, onSquareClick}) {
  );
 }
 
+// ボード全体についての関数(メイン)
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null)); // 初期値は9個のnullを持った配列を初期値とする
   const [xIsNext, setXIsNext] = useState(true); // OかXかを判断
+  const winner = calculateWinner(squares); 
 
   function handleClick (i) {
-    if(squares[i]){
-      return; // 値がnullでなければ何もしない
+    if(squares[i] || winner){
+      return; // 値がnull、もしくはどちらかのプレイヤーが勝利した場合、何も行わない
     }
     const nextSquares = squares.slice(); // sliceメソッドを引数なしで使用して配列をコピー
     if(xIsNext){
@@ -29,8 +32,17 @@ export default function Board() {
     setSquares(nextSquares);
     setXIsNext(!xIsNext);
   }
+  // ゲームが決着したかを確かめ、勝利したプレイヤー、もしくは次のプレイヤーを表示
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
   return( 
     <>
+    <div className="status">{status}</div>
     <div className="board-row"> 
       <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
       <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -53,3 +65,23 @@ export default function Board() {
     )
   }
   
+  // squaresを受け取り勝利判定をする関数
+function calculateWinner(squares) {
+  const lines = [ // 勝利条件の設定
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}

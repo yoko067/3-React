f3cec380c78f755b6b8f970b441938de4877e8f8
yoko067 <ee217067@meiji.ac.jp>
2024-05-24 23:04:40 +0900

    <add>: 過去の状態にさかのぼり、その地点からリスタートできる機能の追加

--- react-intro/tutorial2/src/App.js
+++ react-intro/tutorial2/src/App.js
@@ -65,18 +65,43 @@ function Board({xIsNext, squares, onPlay}) {
 export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]); // [Array(9).fill(null)] -> []によって9つの要素を持った配列をさらに配列化する
-  const currentSquares = history[history.length - 1];
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
+  
   function handlePlay(nextSquares) {
-    setHistory([...history, nextSquares]); // ...(スプレッド構文): historyすべての項目を表す
+    // 過去にさかのぼった場合、戻った時点から先に上書きしていくように進める
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares]; // ...(スプレッド構文): historyすべての項目を表す
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);  
     setXIsNext(!xIsNext);
   }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li key="move">
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
   return (
     <div className="game">
       <div className="game-board">
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
-        <ol>{/*TODO*/}</ol>
+        <ol>{moves}</ol>
       </div>
     </div>
   );
